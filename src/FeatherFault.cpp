64045f1a7f4bf005f8ea7fb12cfdc2410df65c97
Noah Laptop <noah@koontzs.com>
2020-02-27 14:16:44 -0800

    Fixed flash not writing correctly, wrote handler for stackoverflow

--- src/FeatherFault.cpp
+++ src/FeatherFault.cpp
@@ -1,23 +1,26 @@
 #include "FeatherFault.h"
 
 /** Allocate 512 bytes in flash for our crash logs */
-__attribute__((__aligned__(256))) static volatile const uint8_t FeatherFaultFlash[256] = {};
+__attribute__((__aligned__(256))) _Pragma("location=\"FLASH\"") static const uint8_t FeatherFaultFlash[256] = { 0 };
+const void* FeatherFaultFlashPtr = FeatherFaultFlash;
 
 struct FaultDataPrivate {
     char marker[31] = "FeatherFault Data Here! Cause:";
     FeatherFault::FaultCause cause;
     char marker2[8] = "My Bad:";
     uint8_t mybad;
-    char marker3[8] = "Line #:";
+    char marker3[8] = "Fail #:";
+    uint32_t failnum;
+    char marker4[8] = "Line #:";
     int32_t line;
-    char marker4[6] = "File:";
+    char marker5[6] = "File:";
     char file[64]; // may be corrupted if mybad is true
 };
 
 typedef union {
     struct FaultDataPrivate data;
-    uint32_t raw[(sizeof(FaultDataPrivate)+3)/4]; // rounded to the nearest 4 bytes
-    uint8_t debug_raw[sizeof(FaultDataPrivate)];
+    uint32_t raw_u32[(sizeof(FaultDataPrivate)+3)/4]; // rounded to the nearest 4 bytes
+    uint8_t raw_u8[sizeof(FaultDataPrivate)];
 } FaultData_t;
 
 static const uint32_t pageSizes[] = { 8, 16, 32, 64, 128, 256, 512, 1024 };
@@ -29,10 +32,28 @@ static volatile int last_line = 0;
 /** Global varible to store the last filename. Do not change manually! */
 static volatile const char* last_file = "";
 
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+static int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+
 /**
  * Write fault data to flash
  */
-void FeatherFault::HandleFault(const FeatherFault::FaultCause cause) {
+[[ noreturn ]] void FeatherFault::HandleFault(const FeatherFault::FaultCause cause) {
     // TODO: read the stack?
     // Create a fault data object, and populate it
     FaultData_t trace = { {} };
@@ -51,14 +72,42 @@ void FeatherFault::HandleFault(const FeatherFault::FaultCause cause) {
     }
     else 
         trace.data.file[0] = '\0'; // Corrupted!
+    // read the failure number from flash, and write it + 1
+    trace.data.failnum = ((FaultData_t*)FeatherFaultFlashPtr)->data.failnum + 1;
     // Write that fault data object to flash
-    volatile void* flash_unsafe = (volatile void*)FeatherFaultFlash;
-    volatile uint32_t* flash_safe = (volatile uint32_t*)flash_unsafe;
-    for (uint32_t i = 0; i < sizeof(trace.raw) / 4; i++) {
-        // write!
-        *(flash_safe++) = trace.raw[i];
+    volatile uint32_t* flash_u32 = (volatile uint32_t*)FeatherFaultFlashPtr;
+    volatile uint8_t* const flash_u8 = (volatile uint8_t*)FeatherFaultFlashPtr;
+    // determine page size
+    const uint32_t pagesize = pageSizes[NVMCTRL->PARAM.bit.PSZ];
+    // iterate!
+    const size_t pagewords = pagesize / 4;
+    const size_t pagerows = pagesize * 4;
+    // erase our memory
+    for (size_t i = 0; i < sizeof(trace.raw_u32); i += pagerows) {
+        NVMCTRL->ADDR.reg = ((uint32_t)&(flash_u8[i])) / 2;
+        NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_ER;
+        while (!NVMCTRL->INTFLAG.bit.READY) { }
     }
-    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP;
+    // Disable automatic page write
+    NVMCTRL->CTRLB.bit.MANW = 1;
+    // iterate!
+    const size_t writelen = sizeof(trace.raw_u32) / 4;
+    size_t idx = 0;
+    while (idx < writelen) {
+        // Execute "PBC" Page Buffer Clear
+        NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_PBC;
+        while (NVMCTRL->INTFLAG.bit.READY == 0) { }
+        // write!
+        const size_t min_idx = (writelen - idx) > pagewords ? pagewords : (writelen - idx);
+        for (size_t i = 0; i < min_idx; i++)
+            *(flash_u32++) = trace.raw_u32[idx++];
+        // flush the page if needed (every pagesize words and the last run)
+        NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD_WP;
+        while (NVMCTRL->INTFLAG.bit.READY == 0) { }
+    }
+    // All done! the chip will now reset
+    NVIC_SystemReset();
+    while(true);
 }
 
 static void WDTReset() {
@@ -73,8 +122,6 @@ static void WDTReset() {
     WDT->INTFLAG.bit.EW  = 1;        // Clear interrupt flag
     // Handle fault!
     HandleFault(FeatherFault::FAULT_HUNG);
-    // manually reset the board 
-    // while(true) {}
 }
 
 void FeatherFault::Init(const FeatherFault::WDTTimeout timeout) {
@@ -103,10 +150,8 @@ void FeatherFault::Init(const FeatherFault::WDTTimeout timeout) {
     WDT->INTENSET.bit.EW   = 1;
     // Period = twice
     WDT->CONFIG.bit.PER    = static_cast<uint8_t>(timeout);
-    Serial.println(WDT->CONFIG.bit.PER);
     // Set time of interrupt 
     WDT->EWCTRL.bit.EWOFFSET = static_cast<uint8_t>(timeout) - 1;
-    Serial.println(WDT->EWCTRL.bit.EWOFFSET);
     // Disable window mode
     WDT->CTRL.bit.WEN      = 0;
     // Sync CTRL write
@@ -127,6 +172,10 @@ void FeatherFault::Mark(const int line, const char* file) {
     last_line = line;
     last_file = file;
     is_being_written.store(false);
+    // check for a stackoverflow
+    const int mem = freeMemory();
+    if (mem < 0 || mem > 60000)
+        HandleFault(FeatherFault::FAULT_STACKOVERFLOW);
 }
 
 /**
@@ -134,23 +183,24 @@ void FeatherFault::Mark(const int line, const char* file) {
  */
 void FeatherFault::PrintFault(Print& where) {
     // Load the fault data from flash
-    FaultData_t trace = { {} };
-    memcpy(&trace.raw, (const void*)FeatherFaultFlash, sizeof(trace));
+    const FaultData_t* trace = (FaultData_t*)FeatherFaultFlashPtr;
     // print it the printer
-    if (trace.data.cause != FeatherFault::FAULT_NONE) {
+    if (trace->data.cause != FeatherFault::FAULT_NONE) {
         where.print("Fault! Cause: ");
-        switch (trace.data.cause) {
+        switch (trace->data.cause) {
             case FeatherFault::FAULT_HUNG: where.println("HUNG"); break;
             case FeatherFault::FAULT_HARDFAULT: where.println("HARDFAULT"); break;
             case FeatherFault::FAULT_STACKOVERFLOW: where.println("STACKOVERFLOW"); break;
             default: where.println("Corrupted");
         }
         where.print("FeatherFault's error: ");
-        where.println(trace.data.mybad ? "Yes" : "No");
+        where.println(trace->data.mybad ? "Yes" : "No");
         where.print("Line: ");
-        where.println(trace.data.line);
+        where.println(trace->data.line);
         where.print("File: ");
-        where.println(trace.data.file);
+        where.println(trace->data.file);
+        where.print("# of failures since upload: ");
+        where.println(trace->data.failnum);
     }
     else
         where.println("No fault");
